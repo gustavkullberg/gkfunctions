e89dd4b83565c7df59b94f07d1442e04b84a45f5
Gustav Kullberg <gustav.kullberg@gmail.com>
2018-10-28 14:08:05 +0100

    update

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -27,7 +27,7 @@ namespace Company.Function
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name} \n mvh Gustav")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request buddy");
         }
     }
 }
